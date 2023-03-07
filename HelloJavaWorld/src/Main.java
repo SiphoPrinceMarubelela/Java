5f47ac0984597de7e45028bfedf23a41b6041567
SiphoPrinceMarubelela <siphoprincemarubelela@gmail.com>
2023-03-07 23:33:25 +0200

    uploaded first java program for good luck!

---
+++ HelloJavaWorld/src/Main.java
@@ -0,0 +1,20 @@
+//comments are done using a double forward slash
+//escape sequences: - they are preceded by a backslash
+//					- \n : newline character
+//					- \t : tab 
+//					- \"": quotation mark
+
+/*
+ * This is a multiline comment
+ * It spans over the next line
+ * */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java World!\n");
+		System.out.println("I love pizza!");
+	}
+
+}
